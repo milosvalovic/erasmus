cb4627b543be3a6f9e44d4476e0d2bd5877b75db
Bence <m.bence05@gmail.com>
2019-10-20 17:43:46 +0200

    aktualné ponuky mobilít hotovo

---
+++ public/js/client/app.js
@@ -0,0 +1,9 @@
+$('document').ready(function () {
+    const typed = new Typed('#typed', {
+        strings: ['pobyty', 'stáže', 'prednáškové pobyty', 'školenia'],
+        backSpeed: 70,
+        typeSpeed: 70,
+        loop: true,
+        showCursor: false,
+    });
+});
--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -2,7 +2,8 @@
 <html lang="sk">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('fav/apple-touch-icon.png')}}">
@@ -11,15 +12,16 @@
 
     <link rel="manifest" href="<?php echo asset('fav/site.webmanifest')?>">
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate-css/animate.min.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/bootstrap.min.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/font-awesome/font-awesome.min.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/animate-css/animate.min.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/bootstrap.min.css') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/font-awesome/font-awesome.min.css') }}"/>
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/client/app.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/client/app.css') }}"/>
 
     <script type="text/javascript" src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('vendor/bootstrap/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('vendor/jquery-jvectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
+    <script type="text/javascript"
+            src="{{ asset('vendor/jquery-jvectormap/jquery-jvectormap-2.0.3.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('vendor/typed-js/typed.min.js') }}"></script>
 
     <script type="text/javascript" src="{{ asset('js/client/app.js') }}"></script>
@@ -30,7 +32,9 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container-fluid">
         <a class="navbar-brand" href="{{ url('/') }}"><i class="ukf-logo"></i></a>
-        <button class="navbar-toggler navbar-toggler-right" id="toogler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+        <button class="navbar-toggler navbar-toggler-right" id="toogler" type="button" data-toggle="collapse"
+                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -64,9 +68,9 @@
             <div class="col-lg-6"></div>
             <div class="col-lg-6 col-sm-12">
                 <div class="search-modal">
-                    <h1>Vyhľadávajte a <br>prihlaste sa na pobyty.</h1>
+                    <h1>Vyhľadávajte a <br>prihlaste sa na <span id="typed"></span>.</h1>
                     <div class="input-items">
-                        <form action="POST" enctype="application/x-www-form-urlencoded">
+                        <form action="{{ url('/') }}" method="POST" enctype="application/x-www-form-urlencoded">
                             <label for="krajina">KRAJINA</label>
                             <input type="text" name="country" id="krajina" value="" placeholder="Nemecko">
                             <label for="pobyt">TYP POBYTU</label>
@@ -75,7 +79,8 @@
                             </select>
                             <br/>
                             <label for="univerzita">UNIVERZITA</label>
-                            <input type="text" name="university" id="univerzita" value="" placeholder="Technische Universität">
+                            <input type="text" name="university" id="univerzita" value=""
+                                   placeholder="Technische Universität">
                             <label for="hodnotenie">HODNOTENIE</label>
                             <select name="rating" id="hodnotenie">
                                 <option value="5">5</option>
@@ -94,5 +99,89 @@
         </div>
     </div>
 </header>
+<section class="page-section" id="current-opportunities">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="section-heading">Naše aktualné ponuky mobilít</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/a.jpg') }}" alt="Popis mobility a" title="Popis mobility a"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">Europa - studijny pobyt</a>
+                <p class="current-opportunitie-date">prihlasky do 13.10.2019</p>
+                <p class="current-opportunitie-comments">36 kommentarov</p>
+            </div>
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/b.jpg') }}" alt="Popis mobility b" title="Popis mobility b"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">Tchaj-wan, Hongkong, Čína</a>
+                <p class="current-opportunitie-date">prihlášky do 20.11.2019</p>
+                <p class="current-opportunitie-comments">10 kommentarov</p>
+            </div>
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/c.jpg') }}" alt="Popis mobility c" title="Popis mobility c"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">Rusko - St. Petersburg</a>
+                <p class="current-opportunitie-date">prihlášky do 13.11.2019</p>
+                <p class="current-opportunitie-comments">23 kommentarov</p>
+            </div>
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/d.jpg') }}" alt="Popis mobility d" title="Popis mobility d"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">Malajsie</a>
+                <p class="current-opportunitie-date">prihlášky do 12.12.2019</p>
+                <p class="current-opportunitie-comments">45 kommentarov</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/e.gif') }}" alt="Popis mobility e" title="Popis mobility e"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">Taiwan</a>
+                <p class="current-opportunitie-date">prihlášky do 09.10.2019</p>
+                <p class="current-opportunitie-comments">33 kommentarov</p>
+            </div>
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/f.jpg') }}" alt="Popis mobility f" title="Popis mobility f"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">USA</a>
+                <p class="current-opportunitie-date">prihlášky do 05.11.2019</p>
+                <p class="current-opportunitie-comments">13 kommentarov</p>
+            </div>
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/g.jpg') }}" alt="Popis mobility g" title="Popis mobility g"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">Latinská Amerika</a>
+                <p class="current-opportunitie-date">prihlášky do 11.11.2019</p>
+                <p class="current-opportunitie-comments">35 kommentarov</p>
+            </div>
+            <div class="col-xl-3 col-sm-6 text-center">
+                <img src="{{ asset('uploads/temp/h.jpg') }}" alt="Popis mobility h" title="Popis mobility h"
+                     class="rounded">
+                <br/>
+                <a href="{{ url('/') }}" class="current-opportunitie-name">Velká Británie</a>
+                <p class="current-opportunitie-date">prihlášky do 12.18.2019</p>
+                <p class="current-opportunitie-comments">5 kommentarov</p>
+            </div>
+        </div>
+        <div class="row" id="more-opportunitie">
+            <div class="container">
+                <div class="cold-md-12 text-right">
+                    <a href="{{ url('/') }}">Ďalšie pobyty ...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 </body>
 </html>
