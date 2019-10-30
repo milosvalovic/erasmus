a03bd1fb1c7093333c9236bdd08c061b6046e2f5
Bence <m.bence05@gmail.com>
2019-10-30 22:36:56 +0100

    obrazovka najdite svoj ucet hotova

--- resources/views/client/account.blade.php
+++ resources/views/client/account.blade.php
@@ -2,5 +2,5 @@
 @section('title', 'Účet')
 @section('css', asset('css/client/account.css'))
 @section('masthead')
-   @include('client.layout.account.login')
+   @include('client.layout.account.forget_password')
 @endsection
