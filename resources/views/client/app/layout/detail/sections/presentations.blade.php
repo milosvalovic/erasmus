485ebd38aab70ab03a60d48499c4612998a6c3ba
Bence <m.bence05@gmail.com>
2019-11-02 20:42:50 +0100

    bug fix tabulky prezentacie

--- resources/views/client/app/layout/detail/sections/presentations.blade.php
+++ resources/views/client/app/layout/detail/sections/presentations.blade.php
@@ -7,21 +7,21 @@
             <thead class="thead-color">
             <tr>
                 <th class="text-left">@lang('app.detail_presentations_table_title_school')</th>
-                <th>@lang('detail_presentations_table_title_file')</th>
+                <th>@lang('app.detail_presentations_table_title_file')</th>
             </tr>
             </thead>
             <tbody>
             <tr>
                 <td class="text-left">Beykent University, Istanbul, Turecko</td>
-                <td><a href="{{ url('/') }}">@lang('detail_presentations_table_file_download')</a></td>
+                <td><a href="{{ url('/') }}">@lang('app.detail_presentations_table_file_download')</a></td>
             </tr>
             <tr>
                 <td class="text-left">Ege University, Izmir, Turecko</td>
-                <td><a href="{{ url('/') }}">@lang('detail_presentations_table_file_download')</a></td>
+                <td><a href="{{ url('/') }}">@lang('app.detail_presentations_table_file_download')</a></td>
             </tr>
             <tr>
                 <td class="text-left">University of Cyprus</td>
-                <td><a href="{{ url('/') }}">@lang('detail_presentations_table_file_download')</a></td>
+                <td><a href="{{ url('/') }}">@lang('app.detail_presentations_table_file_download')</a></td>
             </tr>
             </tbody>
         </table>
