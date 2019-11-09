b8d4b4b5ce22ba110efed0889e6357aa66e31d36
Bence <m.bence05@gmail.com>
2019-11-09 16:04:01 +0100

    bug fix kontaktu pre sekciu mobility

--- resources/views/client/app/mobilities.blade.php
+++ resources/views/client/app/mobilities.blade.php
@@ -9,7 +9,7 @@
     <div class="content">
         @include('client.app.layout.mobilities.mobility')
         @include('client.app.layout.home.newsletter')
-        @include('client.app.layout.home.contact')
+        @include('client.app.layout.home.contact', ["contact" => array_chunk($contact, 2), "office_hours" => $office_hours, "address" => $address])
         @include('layout.client.app.footer')
     </div>
 @endsection
