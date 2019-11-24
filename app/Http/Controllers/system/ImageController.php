7db9c54a9f73e43ba725a4e3f4bb490a83a73d43
milosvalovic <valvovic.milos1@gmail.com>
2019-11-24 17:27:18 +0100

    Update ImageController.php

--- app/Http/Controllers/system/ImageController.php
+++ app/Http/Controllers/system/ImageController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers\system;
 
 
+use App\Models\Images;
 use Illuminate\Routing\Controller;
 
 class ImageController extends Controller
 {
     public function images()
     {
-        return view('system.images_admin');
+        $images = Images::get();
+        return view('system.images_admin')->with(["images" => $images]);
     }
+
+    public function deleteImage($id){
+        $image = Images::find($id);
+        $fullImage = $image->url;
+        $thumbImage = $image->thumb_url;
+
+        if(file_exists($fullImage)){
+            @unlink($fullImage);
+        }
+        if(file_exists($thumbImage)){
+            @unlink($thumbImage);
+        }
+        
+        $image->delete();
+        return back();
+    }
+
 
 }
