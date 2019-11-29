74bbe189ea29c857389975f7d4067a071e00f69a
EduardGabel <eduardgabel.praca@gmail.com>
2019-11-29 15:32:43 +0100

    Admin - Office Hours >> deleted pagination

--- resources/views/system/office_hours_admin.blade.php
+++ resources/views/system/office_hours_admin.blade.php
@@ -19,7 +19,6 @@
                 <div class="admin-office-hours-title">
                     <h2>Spravovanie rolí</h2>
                 </div>
-
                 <table class="table admin-table">
                     <thead>
                     <tr>
@@ -46,25 +45,8 @@
                     @endforeach
                     </tbody>
                 </table>
-
-                <nav class="admin-users-pagination" aria-label="Page navigation example">
-                    <ul class="pagination">
-                        <li class="page-item disabled">
-                            <a class="page-link" href="#" tabindex="-1"><</a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#"> > </a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </div>
-
-
-
 
         @include('system.include.footer')
     </div>
