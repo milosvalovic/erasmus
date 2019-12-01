f2d58de064382c76f5466c748f0b44489f384448
milosvalovic <valovic.milos1@gmail.com>
2019-12-01 04:41:50 +0100

    prepojenie backendu a frontentu mobilít

--- resources/views/system/mobility_admin.blade.php
+++ resources/views/system/mobility_admin.blade.php
@@ -30,7 +30,7 @@
                                 <th scope="col">Krajina</th>
                                 <th scope="col">Typ</th>
                                 <th scope="col">Kategória</th>
-                                <th scope="col">Štipendium</th>
+                                <th scope="col">Štipendium (€)</th>
                                 <th scope="col" class="user-form-actions">Akcie</th>
                             </tr>
                             </thead>
@@ -38,16 +38,16 @@
                             @foreach($mobilities as $mobility)
                                 <tr>
                                     <th scope="row">{{ $mobility->ID  }}</th>
-                                    <td>{{ $mobility->partner_university_ID }}</td>
-                                    <td></td>
-                                    <td>{{ $mobility->mobility_types_ID }}</td>
-                                    <td>{{ $mobility->category_ID }}</td>
-                                    <td>{{ $mobility->grant }}</td>
+                                    <td>{{ $mobility->university->name}}</td>
+                                    <td>{{ $mobility->country->name}}</td>
+                                    <td>{{ $mobility->mobility_type->name}}</td>
+                                    <td>{{ $mobility->category->name}}</td>
+                                    <td>{{ $mobility->grant}}</td>
                                     <th scope="row">
                                         <a href="{{ action('system\MobilityController@mobilityEditShow',['id' => $mobility->ID]) }}">
                                             <button type="button" class="btn btn-outline-warning">Upraviť</button>
                                         </a>
-                                        <a href="/delete-user/'number'">
+                                        <a href="{{route('deleteMobility',['id' => $mobility->ID])}}">
                                             <button type="button" class="btn btn-outline-danger">Odstrániť</button>
                                         </a>
                                     </th>
@@ -56,54 +56,65 @@
                             </tbody>
                         </table>
                         <nav class="admin-users-pagination">
-                            {{--TODO Here pagination--}}
+                            {{$mobilities->links()}}
                         </nav>
                     </div>
                 </div>
 
                 <div class="col-xs-12 col-md-3 admin-add-new-item-div">
-                    <form method="post" class="form-add-faq" id="formNewFaq" action="">
+                    <form method="post" class="form-add-faq" id="formNewMobility" action="{{route('addMobility')}}">
                         <h3 class="form-title">Pridať mobilitu</h3>
-
+                        @if (session('error'))
+                            <div class="alert alert-danger">
+                                <h5>{{session('error')}}</h5>
+                            </div>
+                        @endif
                         <div class="form-group">
                             <label for="addMobilityUniversity">Partnerská univerzita: </label>
-                            <select name="addMobilityUniversityId" class="form-control admin-form-input" id="addMobilityUniversity" required>
-                                {{--                            @foreach($universities as $university)--}}
-                                {{--                                <option value="{{$university->ID}}"--}}
-                                {{--                                        @if($university->ID == $mobility->partner_university_ID) selected="selected" @endif>{{$university->name}}</option>--}}
-                                {{--                            @endforeach--}}
+                            <select name="university_ID" class="form-control admin-form-input"
+                                    id="addMobilityUniversity" required>
+                                @foreach($universities as $university)
+                                    <option value="{{$university->ID}}">{{$university->name . ' (' . $university->country->name . ')'}}</option>
+                                    @endforeach--}}
                             </select>
                         </div>
 
                         <div class="form-group">
                             <label for="addMobilityCategory">Kategória: </label>
-                            <select name="addMobilityCategoryId" class="form-control admin-form-input" id="addMobilityCategory" required>
-                                {{--                            @foreach($universities as $university)--}}
-                                {{--                                <option value="{{$university->ID}}"--}}
-                                {{--                                        @if($university->ID == $mobility->partner_university_ID) selected="selected" @endif>{{$university->name}}</option>--}}
-                                {{--                            @endforeach--}}
+                            <select name="category_ID" class="form-control admin-form-input" id="addMobilityCategory"
+                                    required>
+                                @foreach($categories as $category)
+                                    <option value="{{$category->ID}}">{{$category->name}}</option>
+                                @endforeach
                             </select>
                         </div>
 
                         <div class="form-group">
                             <label for="addMobilityType">Typ: </label>
-                            <select name="addMobilityTypeId" class="form-control admin-form-input" id="addMobilityType" required>
-                                {{--                            @foreach($universities as $university)--}}
-                                {{--                                <option value="{{$university->ID}}"--}}
-                                {{--                                        @if($university->ID == $mobility->partner_university_ID) selected="selected" @endif>{{$university->name}}</option>--}}
-                                {{--                            @endforeach--}}
+                            <select name="type_ID" class="form-control admin-form-input" id="addMobilityType" required>
+                                @foreach($types as $type)
+                                    <option value="{{$type->ID}}">{{$type->name}}</option>
+                                @endforeach
                             </select>
                         </div>
 
                         <div class="form-group">
-                            <label for="addNewUniversityGrand">Výška štipendia:</label>
-                            <input type="text" class="form-control admin-form-input" id="addNewUniversityGrand"
-                                   placeholder="725€" name="addMobilityGrand" required>
+                            <label for="addNewUniversityInfo">Popis:</label>
+                            <textarea class="form-control admin-form-input" id="addNewUniversityInfo"
+                                      placeholder="" name="info" form="formNewMobility" required></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="addNewUniversityGrant">Výška štipendia:</label>
+                            <input type="number" step="1" class="form-control admin-form-input"
+                                   id="addNewUniversityGrant"
+                                   placeholder="725€" name="grant" required>
                         </div>
 
                         <div class="form-group-button">
                             <button type="submit" class="btn btn-outline-primary btn-add">Pridať</button>
                         </div>
+                        {{csrf_field()}}
                     </form>
                 </div>
             </div>
