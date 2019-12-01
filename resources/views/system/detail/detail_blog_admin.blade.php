9cb6995cb327b00ef7e2875a4488794d97e43b3c
EduardGabel <eduardgabel.praca@gmail.com>
2019-12-01 22:13:54 +0100

    Admin - BlogDetail, detail done

--- resources/views/system/detail/detail_blog_admin.blade.php
+++ resources/views/system/detail/detail_blog_admin.blade.php
@@ -6,7 +6,7 @@
 
         <div class="admin-title">
             <div class="admin-title-text">
-                <h1>Detail blogu</h1>
+                <h1>Detail</h1>
             </div>
             <div class="admin-title-user">
                 <p>Eduard Gábel</p>
@@ -15,11 +15,38 @@
         </div>
 
         <div class="admin-content">
+            <div class="detail-container">
+                <div class="blog-detail-title">
+                    <div class="blog-detail--title left-side-title">
+                        <h1>{{ $blog->title }}</h1>
+{{--                        <a class="a" href="#">--}}
+{{--                            <p>{{ $blog->users_season_ID }}</p>--}}
+{{--                        </a>--}}
+                    </div>
 
+                    <div class="blog-detail--title">
+                        <p>Zverejnený dňa / {{ $blog->publish_date }}</p>
+                        @if( $blog->status==1 )
+                        <div class="blog-detail-active-status"></div>
+                        @else
+                        <div class="blog-detail-active-not-status"></div>
+                        @endif
+                    </div>
+
+                </div>
+                <div class="blog-detail-content">
+                    {!! $blog->article !!}
+                </div>
+            </div>
         </div>
 
         @include('system.include.footer')
-
     </div>
+
+    <script>
+        $('#summernote').summernote({
+            height: 'auto'
+        });
+    </script>
 @endsection
 
