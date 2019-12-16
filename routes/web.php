6d10334cc23f49e8f7b52a0d0757d82133af9c10
milosvalovic <valovic.milos1@gmail.com>
2019-12-16 15:54:57 +0100

    admin welcome screen

--- routes/web.php
+++ routes/web.php
@@ -69,6 +69,7 @@ Route::get('/novinky/{email}/{hash}','Auth\Newsletter@signOut');
 /*------------------------------- Admin routes---------------------------------------------------------------------------------------------------------------------*/
 Route::group(['middleware' => ['web','auth','admin&org']], function () {
 Route::get('/admin', 'system\SystemController@system');
+Route::get('/admin/charts', 'system\SystemController@charts');
 
 /*------User routes-------------*/
 Route::get('/admin/users', 'system\UserController@users');
